7a6dcca5855fc5f388b7de7b7f7f2d64c188d8f2
eclipse4j <eclipse4j@GrissomPC>
2017-03-05 20:14:53 +0900

    Memcached

--- src/main/java/com/tistory/eclipse4j/DockerSpringappsApplication.java
+++ src/main/java/com/tistory/eclipse4j/DockerSpringappsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DockerSpringappsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DockerSpringappsApplication.class, args);
-	}
+    public static void main(String[] args) {
+	SpringApplication.run(DockerSpringappsApplication.class, args);
+    }
 }
